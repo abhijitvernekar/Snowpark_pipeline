d4c12e1326b22338cf487ca0f2aa1c8262ac0573
abhijitvernekar <32807852+abhijitvernekar@users.noreply.github.com>
2024-09-08 18:46:00 +0000

    created config file

--- emp_load_to_stg.py
+++ emp_load_to_stg.py
@@ -13,38 +13,21 @@
 import sys
 sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
 from generic_code import code_library
-from schema import source_schema
+from schema import src_stg_schema
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.types import IntegerType, StringType,StructType,DateType,StructField
+import json
 
+config_snow_copy = open('./config/copy_to_snowstg.json', "r")
+config_snow_copy = json.loads(config_snow_copy.read())
+print(config_snow_copy)
 
-connection_parameters = {"account":"kp41433.ap-southeast-1", \
-"user":"pavan", \
-"password": "Abc123123", \
-"role":"ACCOUNTADMIN", \
-"warehouse":"COMPUTE_WH", \
-"database":"DEMO_DB", \
-"schema":"PUBLIC" \
-}
+connection_parameter = open('./config/connection_details.json', "r")
+connection_parameter = json.loads(connection_parameter.read())
 
-config_file = {"Database_name":"DEMO_DB",\
-"Schema_name":"PUBLIC",
-"Target_table":"EMPLOYEE",
-"Reject_table":"EMPLOYEE_REJECTS",
-"target_columns":["FIRST_NAME","LAST_NAME","EMAIL","ADDRESS","CITY","DOJ"],
-"on_error":"CONTINUE",
-"Source_location":"@my_s3_stage/employee/",
-"Source_file_type":"csv"
-}
-    
-# Declare schema for csv file and read data
-schema = StructType([StructField("FIRST_NAME", StringType()),
-StructField("LAST_NAME", StringType()),
-StructField("EMAIL", StringType()),
-StructField("ADDRESS", StringType()),
-StructField("CITY", StringType()),
-StructField("DOJ",DateType())])
 
 session = code_library.snowconnection(connection_parameters)
+
 copied_into_result, qid = code_library.copy_to_table(session,config_file,schema)
 
 print(copied_into_result)
